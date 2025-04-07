a0039be4db3496a4fc62baf7f344500d3feca8c0
SAURABH <saurabhsingh17106@gmail.com>
2025-04-07 20:25:54 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -3,7 +3,7 @@ void bubblesort(int arr[],int n){
     int temp;
     for (int i=0;i<=n-1;i++){
         for(int j=0;j<=n-i-1;i++){
-            if arr[j]=arr[j+1]{
+            if (arr[j]=arr[j+1]){
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
